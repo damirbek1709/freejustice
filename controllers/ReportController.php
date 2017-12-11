abcf0772365bb07150ecfa35f1ccba8d56d29661
Damirbek <damirbek@gmail.com>
2017-12-11 08:26:59 +0600

    recover

--- controllers/ReportController.php
+++ controllers/ReportController.php
@@ -197,7 +197,6 @@ class ReportController extends Controller
         ]);
     }
 
-
     /**
      * Updates an existing Report model.
      * If update is successful, the browser will be redirected to the 'view' page.
